90d3ff199fe56b74b5951de06935a7b3874c6b03
Kaan Enes Çiftçi <kaanenesciftci@gmail.com>
2022-07-17 20:28:11 +0300

    Bug Fix

--- lib/Widgets/product_grid.dart
+++ lib/Widgets/product_grid.dart
@@ -14,8 +14,9 @@ class ProductGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (BuildContext context, int index) => ChangeNotifierProvider(
-        create: (BuildContext context) => products[index],
+      itemBuilder: (BuildContext context, int index) =>
+          ChangeNotifierProvider.value(
+        value: products[index],
         child: ProductItem(),
       ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(

2ad168a4ddb3260080580e980e6f8a5a6b514e63
Kaan Enes Çiftçi <kaanenesciftci@gmail.com>
2022-07-20 20:00:01 +0300

    Add Shoping cart

---
+++ lib/Providers/cart.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items => {..._items};
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCardItem) => CartItem(
+                id: existingCardItem.id,
+                title: existingCardItem.id,
+                quantity: existingCardItem.quantity + 1,
+                price: existingCardItem.price,
+              ));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                quantity: 1,
+                price: price,
+              ));
+    }
+  }
+}

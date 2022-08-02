cec0cc81eb5956ffdca2208577d1fbdefd21a073
Kaan Enes Çiftçi <kaanenesciftci@gmail.com>
2022-08-02 19:53:56 +0300

    Add Edit product screen

---
+++ lib/Screens/edit_product_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  const EditProductScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Product"),
+      ),
+    );
+  }
+}

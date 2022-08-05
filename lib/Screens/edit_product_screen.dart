0b0e3c3e043c3acd3f7ac352f7a0d2766f5dd043
Kaan Enes Çiftçi <kaanenesciftci@gmail.com>
2022-08-05 20:21:52 +0300

    fix Edit product screen memory leak

--- lib/Screens/edit_product_screen.dart
+++ lib/Screens/edit_product_screen.dart
@@ -10,6 +10,13 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +41,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.number,
               focusNode: _priceFocusNode,
+              onFieldSubmitted: (value) {
+                FocusScope.of(context).requestFocus(_descriptionFocusNode);
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(label: Text("Description")),
+              maxLines: 3,
+              keyboardType: TextInputType.multiline,
+              focusNode: _descriptionFocusNode,
             ),
           ],
         )),
